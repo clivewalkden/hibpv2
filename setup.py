caf0aca9abbca8c4503a51185783a0043392bfbf
Jeffrey Mustard <33340749+JeffreyMustard@users.noreply.github.com>
2017-11-04 20:04:14 -0500

    hibpv2 installer
    I added an installer that the old version lacked.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This is the installer for hibp.py
+
+import os
+
+print("Installing the HaveIBeenPwned command line interface...")
+os.system("chmod +x hibp.py")
+print("...")
+print("...")
+print("...")
+print("Thanks for installing the HaveIBeenPwned command line interface! You may use hibp --help to view the help screen!")
+print("")
+print("Please type the following command to begin using hibpv2: (alias hibp='python ~/hibpv2/hibp.py' ) ")
+print("")
