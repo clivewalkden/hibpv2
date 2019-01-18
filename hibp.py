02cda810003c6413de2e60aac8cd3ceff7b6bb70
Clive Walkden <clive@sozodesign.co.uk>
2019-01-18 17:40:44 +0000

    Initial Tweaks to get the code in line

--- hibp.py
+++ hibp.py
@@ -9,21 +9,33 @@ import requests
 import time
 import argparse
 import json
+import os
+import logging
 
 parser = argparse.ArgumentParser(description="Verify if email address has been pwned")
 parser.add_argument("-a", dest="address",
-                  help="Single email address to be checked")
+                    help="Single email address to be checked")
 parser.add_argument("-f", dest="filename",
-                  help="File to be checked with one email addresses per line")
+                    help="File to be checked with one email addresses per line")
+parser.add_argument("-o", dest="output",
+                    help="File to written to")
 args = parser.parse_args()
 
-#declare global vars
-server = "https://haveibeenpwned.com/api/v2/breachedaccount/"
+# declare global vars
+server = "https://haveibeenpwned.com/api/breachedaccount/"
 sslVerify = True
 address = str(args.address)
 filename = str(args.filename)
+output = str(args.output)
 lstEmail = ["info@example.com","example@example.com", "test@test.com"]
 breached_emails = []
+rate = 1.6
+headers = {
+    'user-agent': 'Python HIBP/0.0.1',
+    'api-version': '2'
+}
+data = {}
+
 
 def main():
     if address != "None":
@@ -37,71 +49,80 @@ def main():
             checkAddress(email)
     breach_info()
 
+
 def checkAddress(email):
-    rate = 1.5
-    check=requests.get(server + email + "?includeUnverified=true",
-                 verify=sslVerify)
-    #Check for 404 - 404 meajns that the API returned no results
+    global rate
+    global headers
+    global data
+    logger = init_logging()
+    check = requests.get(server + email + "?includeUnverified=true", verify=sslVerify, headers=headers)
+    # Check for 404 - 404 means that the API returned no results
     if str(check.status_code) == "404":
         print("[✓] " + email + " has not been breached.")
         time.sleep(rate)
         return False
-    #Check for 200 status - 200 means that the API found a breach
+    # Check for 200 status - 200 means that the API found a breach
     elif str(check.status_code) == "200":
         print("[X] " + email + " has been breached!")
+        jsondata = json.loads(check.text)
+        data[email] = {}
+        breaches = {}
+        for x in range(0, len(jsondata)):
+            breaches[jsondata[x]['Name']] = {'url': jsondata[x]['Domain'], 'breachdate': jsondata[x]['BreachDate']}
+
+        data[email] = breaches
         breached_emails.append(email)
         time.sleep(rate)
         return True
-    #Check for HTTP code 429(rate limit)
+    # Check for HTTP code 429(rate limit)
     elif str(check.status_code) == "429":
-        print("[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds")
+        print(
+            "[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds")
         print("    Refer to acceptable use of API: https://haveibeenpwned.com/API/v2#AcceptableUse")
         rate = float(check.headers['Retry-After'])
         time.sleep(rate)
         checkAddress(email)
     else:
         print("[!] Something went wrong while checking " + email)
+        print(check.content)
         time.sleep(rate)
         return True
 
+
 def breach_info():
-    rate = 1.5
-    for x in xrange(0, len(breached_emails)):
-        check=requests.get(server + breached_emails[x]+ "?includeUnverified=true",
-                        verify=sslVerify)
-    #Check for 404 - 404 meajns that the API returned no results
-        if str(check.status_code) == "404":
-                print("failure for checking " + email)
-                time.sleep(rate)
-                #return False
-    #Check for 200 status - 200 means that the API found a breach
-        elif str(check.status_code) == "200":
-                print("Breach info for: " + breached_emails[x])
-                data = json.loads(check.text)
-                #print json.dumps(data, indent=4)
-                for x in xrange(0,len(data)):
-                        name = data[x]['Name']
-                        dataclass=[]
-                        print "!------ " + name + " ------!"
-                        print "Breached data types: "
-                        for i in xrange(0,len(data[0]['DataClasses'])):
-                                dataclass.append(data[0]['DataClasses'][i])
-                                print '\t' +  dataclass[i]
-                #for i in xrange(0,len(data)):
-        #               print data[i]['Title']['Name']
-                time.sleep(rate)
-                #return True
-    #Check for HTTP code 429(rate limit)
-        elif str(check.status_code) == "429":
-                print("[!] Rate limit exceeded, server instructed us to retry after " + check.headers['Retry-After'] + " seconds")
-                print("    Refer to acceptable use of API: https://haveibeenpwned.com/API/v2#AcceptableUse")
-                rate = float(check.headers['Retry-After'])
-                time.sleep(rate)
-                checkAddress(email)
-        else:
-                print("[!] Something went wrong while checking " + email)
-                time.sleep(rate)
-                return True
+    global rate
+    global headers
+    global data
+
+    if len(data):
+        for email, resultdata in data.items():
+            print("Breach info for: " + email)
+            for name, breachdata in resultdata.items():
+                print("!------ " + name + " ------!")
+                print("Information: ")
+                for k, v in breachdata.items():
+                    print('\t' + k + ': ' + v)
+    else:
+        print("[!] Well done there are no breaches in the adddresses supplied")
+        return True
+
+
+def init_logging():
+    rootlogger = logging.getLogger('my_logger')
+
+    LOG_DIR = os.getcwd() + '/' + 'logs'
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+    filehandler = logging.FileHandler("{0}/{1}.log".format(LOG_DIR, "g2"))
+    rootlogger.addHandler(filehandler)
+
+    rootlogger.setLevel(logging.DEBUG)
+
+    consolehandler = logging.StreamHandler()
+    rootlogger.addHandler(consolehandler)
+
+    return rootlogger
+
 
 if __name__ == "__main__":
     main()
